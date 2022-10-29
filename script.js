b4585dff54b443d7d6b8b92a76f3065130159d5f
Ahmed-2020-98 <57391305+Ahmed-2020-98@users.noreply.github.com>
2022-10-29 15:27:49 +0200

    stars

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
+ 
   var xhr =new XMLHttpRequest();
-  xhr.open("GET","https://atec-web-design-table-default-rtdb.firebaseio.com/allStudents.json",true)
+  xhr.open("GET","https://atec-web-design-table-default-rtdb.firebaseio.com/Group1.json",true)
   xhr.onreadystatechange =function(){
     if(xhr.readyState==4){
       if(xhr.status ==200){
@@ -8,16 +9,17 @@
         var data = JSON.parse(this.response);
         // console.log(JSON.parse(data));
         console.log(data);
-        var tableBodyAll=document.querySelector("#allStudentsTable>tbody");
+        var tableBodyAll=document.querySelector("#StudentsTable1>tbody");
         for(student in data){
           // console.log(data[student].Name);
           // console.log(student.Name);
           var row = `
           <tr>
-            <td>0</td>
-            <td>${data[student].Name}</td>
-            <td>${data[student].Degree}</td>
-            <td>0</td>
+            <td></td>
+            <td>${data[student].Star1>=1? `${data[student].Star1} &times; <i class="fa-solid fa-star star"></i>`:""}  </td>
+            <td>${data[student].Name1}</td>
+            <td>${data[student].Degree1>0?`${data[student].Degree1}`:""}</td>
+            <td>${data[student].Total1}</td>
           </tr>
           `;
 
@@ -29,6 +31,7 @@
       else{
         console.log("Unsuccessfully error when get data");
       }
+      
     }
   }
   xhr.send();
